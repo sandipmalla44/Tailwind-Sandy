c2dd12e73ddeaa853d1aacfe59e41a3af48981bc
sandi <sandipmalla44@gmail.com>
2020-12-17 19:39:17 +0545

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,9 +17,31 @@ module.exports = {
           viol: '#9534eb'
         },
       },
+      animation: {
+        bounceOnce: "goShow 0.5s linear",
+        bounceBack: "goDown 0.5s linear",
+        fadeIn: "fadeIn 2s ease-in forwards"
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        },
+        goShow: {
+          "0%": { transform: 'scale(0)' },
+          "50%": { transform: 'scale(0.5)' },
+          "100%": { transform: 'scale(1)' }
+        },
+        goDown: {
+          "0%": { transform: 'scale(1)' },
+          "50%": { transform: 'scale(0.5)' },
+          "100%": { transform: 'scale(0)' }
+        },
+      },
     },
   },
   variants: {
+    animation: ["motion-safe"],
     extend: {},
   },
   plugins: [],
