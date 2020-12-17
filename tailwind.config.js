1136fdb951d96487ebd8cb906e2c289ac3bf2248
sandi <sandipmalla44@gmail.com>
2020-12-17 11:00:37 +0545

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        pink: {
+          reddish: '#ff306e'
+        },
+        green: {
+          brightG: '#11f255'
+        },
+        blue: {
+          bluish: '#5933d6'
+        },
+        voilet: {
+          viol: '#9534eb'
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
